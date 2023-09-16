248151dd319142089017ebd90480b833ae95a433
ryokatakahashi <ryonryon.xxxx@gmail.com>
2023-09-16 09:30:24 +0000

    bookers1-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
 
+  def top
+  end
+
   def index
     @books = Book.all
     @book = Book.new
@@ -26,15 +29,19 @@ class BooksController < ApplicationController
 
   def update
   	@book = Book.find(params[:id])
-  	@book.update(book_params)
-  	flash[:notice] = "successfully"
-  	redirect_to book_path(@book)
+  	if @book.update(book_params)
+  	  flash[:notice] = 'Successfully updated'
+  	  redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to 'books_path'
+  	flash[:notice] = 'Successfully deleted'
+  	redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => "books#index"
+  root to:  "homes#top"
   
   resources :books
 end
